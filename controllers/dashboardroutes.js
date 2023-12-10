27484eb0a9dc62805fe137ee3ca209dca2419ab9
Dasantia89 <143134358+Dasantia89@users.noreply.github.com>
2023-12-10 14:09:01 -0500

    added ability to delete a post

--- controllers/dashboardroutes.js
+++ controllers/dashboardroutes.js
@@ -2,54 +2,67 @@ const router = require('express').Router();
 const { User, Post } = require('../models');
 
 router.get('/', async (req, res) => {
-    try {
-    
-      const postsData = await Post.findAll({
-        where: {
-            user_id: req.session.user
-        },
-        include: [
-          {
-            model: User,
-            attributes: ['username'],
-          },
-        ],
-      });
+  try {
 
-      const posts = postsData.map((post) =>
-        post.get({ plain: true })
-      );
-      console.log(posts);
-      res.render('dashboard', {
-        posts,
-        loggedIn : req.session.loggedIn,
-        name : req.session.name,
-        user_id : req.session.user
-      })
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    };
-  });
+    const postsData = await Post.findAll({
+      where: {
+        user_id: req.session.user
+      },
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    const posts = postsData.map((post) =>
+      post.get({ plain: true })
+    );
+    console.log(posts);
+    res.render('dashboard', {
+      posts,
+      loggedIn: req.session.loggedIn,
+      name: req.session.name,
+      user_id: req.session.user
+    })
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  };
+});
 
 // CREATE new post
 router.post('/', async (req, res) => {
-    try {
-      const dbUserData = await Post.create({
-        title: req.body.title,
-        content: req.body.content,
-        created: req.body.time,
-        user_id: req.body.id
-      });
+  try {
+    const dbUserData = await Post.create({
+      title: req.body.title,
+      content: req.body.content,
+      created: req.body.time,
+      user_id: req.body.id
+    });
 
-        res.status(200).json(dbUserData);
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
+    res.status(200).json(dbUserData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
+router.delete('/', async (req, res) => {
+  try {
+    const dbUserData = await Post.destroy({
+      where: {
+        id: req.body.id
+      }
+    });
 
+    res.status(200).json(dbUserData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 
 
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,10 +1,11 @@
 const newBtn = document.querySelector('#newPost');
 const postDiv = document.querySelector('#postContainer');
+var delBtns = document.querySelectorAll('.delBtn');
+var updBtns = document.querySelectorAll('.updBtn');
 
 newBtn.addEventListener("click", newPost);
-
-function newPost (){
-    postDiv.innerHTML = `<div class="newpost-card">
+function newPost() {
+  postDiv.innerHTML = `<div class="newpost-card">
     <h2 class="page-title">New Post</h2>
     <form class="form newPost-form">
       <div class="form-group">
@@ -22,30 +23,89 @@ function newPost (){
   </div>`;
 
   document
-  .querySelector('.newPost-form')
-  .addEventListener('submit', postFormHandler);
+    .querySelector('.newPost-form')
+    .addEventListener('submit', postFormHandler);
 }
 
 const postFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const title = document.querySelector('#postTitle').value.trim();
-    const content = document.querySelector('#postContent').value.trim();
-    const id = postDiv.dataset.id;
-    const time = Date.now();
+  event.preventDefault();
 
-    console.log('title: '+ title + ' content: '+ content + ' id: '+id+' time: '+time);
-    if (title && content) {
-      const response = await fetch('/dashboard', {
-        method: 'POST',
-        body: JSON.stringify({ title, content,time ,id }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/dashboard');
-      } else {
-        alert('Failed to create post.');
-      }
+  const title = document.querySelector('#postTitle').value.trim();
+  const content = document.querySelector('#postContent').value.trim();
+  const id = postDiv.dataset.id;
+  const time = Date.now();
+
+  console.log('title: ' + title + ' content: ' + content + ' id: ' + id + ' time: ' + time);
+  if (title && content) {
+    const response = await fetch('/dashboard', {
+      method: 'POST',
+      body: JSON.stringify({ title, content, time, id }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to create post.');
     }
-  };
+  }
+};
+
+delBtns.forEach(btn=> btn.addEventListener("click", delPost))
+
+
+async function delPost (event) {
+  event.stopPropagation();
+  const id = event.target.dataset.postid;
+
+  const response = await fetch('/dashboard', {
+      method: 'DELETE',
+      body: JSON.stringify({ id }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to delete post.');
+    }
+}
+
+updBtns.forEach(btn=> btn.addEventListener("click", updPost))
+
+
+function updPost () {
+postDiv.innerHTML = `<div class="newpost-card">
+    <h2 class="page-title">New Post</h2>
+    <form class="form newPost-form">
+      <div class="form-group">
+        <label for="postTitle">Title:</label>
+        <input class="form-input" type="text" id="postTitle" />
+      </div>
+      <div class="form-group">
+        <label for="postContent">Content:</label>
+        <input class="content"  id="postContent" />
+      </div>
+      <div class="form-group">
+        <button class="btn btn-primary" type="submit">Submit</button>
+      </div>
+    </form>
+  </div>`;
+
+  document
+    .querySelector('.newPost-form')
+    .addEventListener('submit', postFormHandler);
+}
+async function updPost (event) {
+  const response = await fetch('/dashboard', {
+    method: 'PUT',
+    body: JSON.stringify({ id }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert('Failed to delete post.');
+  }
+}
